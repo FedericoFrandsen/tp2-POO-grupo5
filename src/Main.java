ac68ff3c5c558c0cf3eb054288836d1222c7444c
FedericoFrandsen <fedeandersfrandsen@gmail.com>
2025-02-17 19:13:06 -0300

    Arreglo metodo salir

--- src/Main.java
+++ src/Main.java
@@ -260,15 +260,16 @@ public class Main {
                                     break;
                             }
                             break;
-                        case 4:
 
-                            System.out.println("Muchas gracias por usar nuestro programa.");
-                            exit = true;
-                            scanner.close();
-                            break;
-                        default:
-                            System.out.println("Opción no válida. Intente de nuevo.");
                     }
+                case 4:
+
+                    System.out.println("Muchas gracias por usar nuestro programa.");
+                    exit = true;
+                    scanner.close();
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intente de nuevo.");
             }
         }
     }
